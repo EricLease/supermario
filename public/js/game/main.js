636a82533b703e6709c6183a8d4ae5252505a08b
eric <lease.eric@gmail.com>
2019-10-20 21:32:21 -0400

    v0.2.0 - move entire game js structure to "game" subfolder - correct script src path in index.html - rename style.css to game.css - add stubs: editor.html, /css/editor.css, /js/editor/main.js

--- public/js/game/main.js
+++ public/js/game/main.js
@@ -5,7 +5,6 @@ import { PlayerController } from './traits.js';
 import { setupKeyboard } from './input.js'
 import { createLevelLoader, loadFont } from './loaders.js';
 import { loadEntities } from './entities.js';
-import { displayHitboxes } from './debug/functions.js';
 import { createDashboardLayer } from './layers.js';
 
 function createPlayerEnvironment(playerEntity) {
@@ -38,8 +37,6 @@ async function main(canvas) {
         camera.pos.x  = Math.max(0, mario.pos.x - 100);
         level.comp.draw(context, camera);
     };
-
-    //displayHitboxes(level); // DEBUG
 
     level.comp.layers.push(createDashboardLayer(font, playerEnvironment));
     level.entities.add(playerEnvironment);
